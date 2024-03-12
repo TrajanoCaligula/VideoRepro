54941e997c0b0a8b840eca492d22e04bb83af768
Alvaro Rodriguez Rubio <alvaro.rodriguez.rubio@estudiantat.upc.edu>
2024-03-12 22:47:22 +0100

    cleaning

--- src/main/java/isdcm/model/DataForDataBase.java
+++ src/main/java/isdcm/model/DataForDataBase.java
@@ -4,15 +4,11 @@
  */
 package isdcm.model;
 
-/**
- *
- * @author Alvaro
- */
+
 public class DataForDataBase {
     public static final String DB_HOST = "jdbc:derby://localhost:1527/VideosRepository";
     public static final String DB_USER = "JAUVARO";
     public static final String DB_PASSWORD = "AJ1234";
     public static final String USERS_TABLENAME = "USERS";
     public static final String VIDEOS_TABLENAME = "VIDEOS";
-
 }
